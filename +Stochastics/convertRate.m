86dd5306134f9f8bf3ae1825b8ad5e2c196b4887
Pascal Pieters <p.a.pieters@student.tue.nl>
2013-06-11 13:00:45 +0200

    Added formula for rate conversion

--- +Stochastics/convertRate.m
+++ +Stochastics/convertRate.m
@@ -1,9 +1,9 @@
-function kmeso = convertRate( k, volume )
+function kmeso = convertRate( k, volume, reactantscount )
 %CONVERTRATE Converts a macroscopic reaction rate to a mesoscopic rate
 
-NA = 6,022141e23; % Define Avogadros Number
+NA = 6.022141e23; % Define Avogadros Number
 
-kmeso = ;
+kmeso = k*(NA*volume)^(reactantscount-1);
 
 end
 
