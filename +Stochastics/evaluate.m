5d04417f5797fd26649bf230614f8f01fe8fec5e
Pascal Pieters <p.a.pieters@student.tue.nl>
2013-06-11 13:55:31 +0200

    merged

--- +Stochastics/evaluate.m
+++ +Stochastics/evaluate.m
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 function [t,a] = evaluate(M, n, tmax)
-=======
-function [t,a] = evaluate(M, n,tmax)
->>>>>>> 17b484ad3f5d10221150d6500ece1aceac540f1c
-=======
-function [t,a] = evaluate(M, n,tmax)
->>>>>>> 86dd5306134f9f8bf3ae1825b8ad5e2c196b4887
 % STOCHASTICS.EVALUATE runs multiple stochastic simlutions.
 % [t,a] = STOCHASTICS.EVALUATE(M, n) runs n stochastic simulation using the 
 % network defined in struct M. M can be generated using the function
--- +Stochastics/run.m
+++ +Stochastics/run.m
@@ -12,15 +12,7 @@ end
 
 tmax=10;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 [t,a] = Stochastics.simulate(M,tmax);
-=======
-[t,a] = Stochastics.simulate(M,10);
->>>>>>> 17b484ad3f5d10221150d6500ece1aceac540f1c
-=======
-[t,a] = Stochastics.simulate(M,10);
->>>>>>> 86dd5306134f9f8bf3ae1825b8ad5e2c196b4887
 figure(1);
 plot(t,a);
 legend(leg);
@@ -29,15 +21,8 @@ xlabel('Time (s)');
 ylabel('Amount (molecules)');
 clear t a;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 [t,a] = Stochastics.evaluate(M,100,tmax);
-=======
-[t,a] = Stochastics.evaluate(M,100,10);
->>>>>>> 17b484ad3f5d10221150d6500ece1aceac540f1c
-=======
-[t,a] = Stochastics.evaluate(M,100,10);
->>>>>>> 86dd5306134f9f8bf3ae1825b8ad5e2c196b4887
+
 figure(2);
 plot(t,a,'LineStyle','none','Marker','.','MarkerSize',1);
 legend(leg);
