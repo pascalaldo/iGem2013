9f141affa422af2c5877c635d56b53fd4d111afa
Yicong Chen <y.chen.3@student.tue.nl>
2013-06-17 14:45:14 +0200

    Added debug info

--- +FNR/run.m
+++ +FNR/run.m
@@ -3,9 +3,13 @@
 clear x0 tspan;
 
 x0 = [40 5 0 0]; %(uM)
+d('Initial concentrations in uM');
+d(sprintf('O2: %d', x0(1)));
+d(sprintf('FNR: %d', x0(2)));
 
 % Integrate ODEs:
 tspan = [0 5]; %(s)
+d('Simulated time: %d seconds', tspan(2));
 [t,x] = ode15s(@FNR.ode,tspan,x0); %Runge-Kutta
 
 % Plot results:
