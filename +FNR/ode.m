216f505ee6ee9097ef86891c495fd0ff97c303e1
Pascal Pieters <p.a.pieters@student.tue.nl>
2013-07-08 16:09:34 +0200

    Started on the new FNR ODE Model

--- +FNR/ode.m
+++ +FNR/ode.m
@@ -1,21 +1,44 @@
-function f = ode(t,x)
+function f = ode(t,x,oxygen)
 % f = ode(t,x) includes the differential equations used in the model
 % See details of the model in README.txt
 
-% state variables
-a = x(1);       % FNR
-b = x(2);       % O2-FNR
-c = x(3);       % O2
+% Set environment to anaerobic
+env = 2;
 
-% Parameters [uM]
-a = 0.0682;
-b = 408;
-kobs = a*c/(b+c);
+%% State Variables
+x1 = x(1); % FNR mRNA
+x2 = x(2); % Inactive FNR
+x3 = x(3); % Active FNR
 
-% ODEs
-f(1) = -kobs*x(1);
-f(2) = kobs*x(1);
-f(3) = 0;
+%% Parameters [uM]
+% par(1) = off state (aerobic)
+% par(2) = on state (anaerobic)
+
+a1      = 0.0871;
+a1max   = 0.135;
+a21     = 0.484;
+a22     = 4.09;
+b22     = 2.6;
+b1(1)   = 0.838;
+b1(2)   = 0.613;
+b21(1)  = 0.0821;
+b21(2)  = 0.0634;
+b31(1)  = 0.0231;
+b31(2)  = 0.0148;
+g13     = -0.464;
+x4      = 0.196;
+x5      = 0.455;
+x6      = oxygen;
+x3c     = 0.389;
+
+%% ODEs
+if x(3) < x3c
+    f(1) = a1max - b1(env)*x(1);
+else
+    f(1) = a1*(x3^g13) - b1(env)*x(1);
+end
+f(2) = a21*x1 + 2*a22*x3*x6 - b21(env)*x2*x4 - 2*b22*(x2^2)*x5;
+f(3) = b22*(x2^2)*x5 - a22*x3*x6 - b31(env)*x3;
 
 f = f(:);
 
--- +FNR/run.m
+++ +FNR/run.m
@@ -2,18 +2,20 @@
 
 clear x0 tspan;
 
-x0 = [5 0 40]; %(uM) [FNR O2-FNR O2]
-d('Initial concentrations in uM');
-d(sprintf('O2: %d', x0(3)));
-d(sprintf('FNR: %d', x0(1)));
+x0 = [0.16 4.63 0.08]; %(uM) [mRNA inactiveFNR activeFNR]
+%d('Initial concentrations in uM');
+%d(sprintf('O2: %d', x0(3)));
+%d(sprintf('FNR: %d', x0(1)));
+
+oxygen = @(t)((t>5)*0.8);
 
 % Integrate ODEs:
-tspan = [0 5]; %(s)
+tspan = [0 10]; %(s)
 d(sprintf('Simulated time: %d seconds', tspan(2)));
-[t,x] = ode45(@FNR.ode,tspan,x0); %Runge-Kutta
+[t,x] = ode45(@(t,x)FNR.ode(t,x,oxygen(t)),tspan,x0); %Runge-Kutta
 
 % Plot results:
-figure; plot(t,x(:,1),t,x(:,2));
-legend('FNR','O2-FNR');
+figure; plot(t,x);
+legend('mRNA', 'Inactive FNR','Active FNR');
 
 clear x0 tspan;
