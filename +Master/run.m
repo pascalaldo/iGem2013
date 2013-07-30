7c1c970f90284cf29a0bca17a4fd3e832e0d9c08
Pascal Pieters <p.a.pieters@student.tue.nl>
2013-07-30 14:44:16 +0200

    Changed the ODE part of the main loop

--- +Master/run.m
+++ +Master/run.m
@@ -2,18 +2,17 @@
 % This part combines the stochastic model (Decoy) and the ODE model (FNR).
 
 close all
-clear xstep x tt tstepODE tstepDecoy tODE t oxygen i amountPlus M inc;
+clear xstep x tt tstepODE tstep t oxygen i amountPlus M inc;
 
 tt = 10;                       % Assign total simulation time
 x = [0.16 4.63 0.08];          % (uM) [mRNA inactiveFNR activeFNR]
-oxygen = @(t)(00);
+oxygen = 0;
 
 M = Decoy.initialize('data/msb20127-s2.xml');
 
 % Initialization
 i = 1;
 t(i) = 0;
-tODE = 0;
 % inc = 0;
 
 while t(i) < tt
@@ -29,30 +28,25 @@ while t(i) < tt
 %     M.amounts(6,i) = M.amounts(8,i) - M.amounts(7,i);           % P
     
     % run the stochastic simulation
-    [tstepDecoy,amountPlus] = Decoy.stepSim(M);
+    [tstep,amountPlus] = Decoy.stepSim(M);
     M.amounts = [M.amounts amountPlus];
 
     % update the stochastic/master time vector
     i = i + 1;
-    t(i) = t(i-1) + tstepDecoy;
+    t(i) = t(i-1) + tstep;
     
     % run the ODE simulation
-    if i==2                                 % For the first loop
-        [tstepODE,xstep] = ode45(@(t,x)FNR.ode(t,x,oxygen(t)),t,x);
-        tODE = [tODE; tstepODE];            % Update ODE time vector
-        x = [x; xstep];                     % Update ODE species vector
-    else
-        [tstepODE,xstep] = ode45(@(t,x)FNR.ode(t,x,oxygen(t)),t([i-1,i]),x(end,:));
-        tODE = [tODE; tstepODE];            % Update ODE time vector
-        x = [x; xstep];                     % Update ODE species vector
-    end
+    x = [x; x(end,:)+FNR.ode(t(i-1),x(end,:),oxygen)'.*tstep];
 end
 
 % Plot the stochastic result and ODE result in one figure
 % plotyy(tODE,x,t,M.amounts)
-plot(tODE,x)
+figure(1);
+plot(t,x);
+hold on;
 legend('mRNA', 'Inactive FNR','Active FNR');
 
 % Plot the promoter binding ratio
-figure
 stairs(t,M.amounts(7,:)./M.amounts(8,:));
+%stairs(t,M.amounts(1,:));
+hold off;
