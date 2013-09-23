8d1f54ed5db5a6c20a0b139c2de17fa6a801b3c5
Yicong Chen <y.chen.3@student.tue.nl>
2013-09-23 14:01:04 +0200

    Added daily injection

--- +Master/run_PBPK.m
+++ +Master/run_PBPK.m
@@ -7,9 +7,21 @@ clear M P t x k y
 P = PBPK.initialize();
 M = PBPK.getOdeMat(P);
 x0 = zeros(1,P.info.compartmentCnt+2);
+% first injection, day 1
 x0(end) = 750;
-[t,x] = ode45(@(t,x)PBPK.ode(t,x,M),[0 50],x0);
-plot(t,x)
+[t1,x1] = ode45(@(t,x)PBPK.ode(t,x,M),[0 24],x0);
+plot(t1,x1(:,8))
+hold on;
+% second injection, day 2
+x0 = x1(end,:);
+x0(end) = x0(end)+750;
+[t2,x2] = ode45(@(t,x)PBPK.ode(t,x,M),[24 48],x0);
+plot(t2,x2(:,8))
+% third injection, day 3
+x0 = x2(end,:);
+x0(end) = x0(end)+750;
+[t3,x3] = ode45(@(t,x)PBPK.ode(t,x,M),[48 72],x0);
+plot(t3,x3(:,8))
 title('Drug Distribution')
 xlabel('time(h)')
 % axis([0 5 0 800])
@@ -20,8 +32,13 @@ end
 leg = {leg{:}, 'arterial','venous'};
 legend(leg)
 
+t = [t1(1:end-1);t2(1:end-1);t3];
+xtumor = [x1(1:end-1,8);x2(1:end-1,8);x3(:,8)];
+
+
+
 % bacteria part
-k = [0.1939   -0.0555  116.6546];
-y = k(3) * tanh(k(1) * t + k(2));
-figure;
-plot(t,y);
+% k = [0.1939   -0.0555  116.6546];
+% y = k(3) * tanh(k(1) * t + k(2));
+% figure;
+% plot(t,y);
